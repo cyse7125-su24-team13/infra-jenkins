ec3c36c056e712918cda12ecb8df59e9752d18d0
vakiti3010 <145172197+vakiti3010@users.noreply.github.com>
2024-06-07 01:58:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
